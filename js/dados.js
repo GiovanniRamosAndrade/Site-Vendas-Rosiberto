082ab9c4abfe35ba73f1c7a15a3e49054064710b
GiovanniRamosAndrade <ramosgiovanni025@gmail.com>
2026-09-23 17:47:35 -0300

    Atualizar produtos pela vitrine

--- js/dados.js
+++ js/dados.js
@@ -103,22 +103,17 @@ window.VITRINE_DADOS = {
       ]
     },
     {
-      "id": "card-muekg3st-kfbjd1",
+      "id": "card-muekns39-yr34ll",
       "title": "Ventilador",
-      "subtitle": "Bla bla bla",
-      "category": "Decoracao",
-      "img": "https://io.convertiez.com.br/m/lojasedmil/shop/products/images/1539/large/ventilador-de-mesa-2-em-1-turbo-force-vf42-arno-40cm-com-6-pas-127v-preto_12065.jpg",
+      "subtitle": "bla bla bla",
+      "category": "decoracao",
+      "img": "https://m.media-amazon.com/images/I/71NYZsZzB7L._AC_UF894,1000_QL80_.jpg",
       "pinned": false,
       "links": [
         {
           "loja": "Shopee",
-          "nome": "Ventilador",
-          "url": "https://www.amazon.com.br/?tag=admarketbr-20&ref=pd_sl_1dc9de7559fff01940f69b3a46b4f7896d96f2178c21d0a292385dec&mfadid=adm"
-        },
-        {
-          "loja": "Amazon",
-          "nome": "Ventilador",
-          "url": "https://www.amazon.com.br/?tag=admarketbr-20&ref=pd_sl_1dc9de7559fff01940f69b3a46b4f7896d96f2178c21d0a292385dec&mfadid=adm"
+          "nome": "Ventilador bolado",
+          "url": "https://shopee.com.br/?mmp_pid=an_18123280001&uls_trackid=56n6dr2u02jq&utm_campaign=id_ZqxYjuR4E2&utm_content=gxbrshopeeshopeesd----&utm_medium=affiliates&utm_source=an_18123280001&utm_term=fkj7xy12m9ts"
         }
       ]
     }
